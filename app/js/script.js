78836e29fa280fea5bbe4d501454c581b5add616
muadz <goadzaskarul@gmail.com>
2021-03-04 23:57:13 +0700

    add  font

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,10 @@
 (function () {
   const calendarBtn = getById("g-calendar-btn");
+  const galleryVid1 = getById("gallery_vid_1");
+  const galleryVid2 = getById("gallery_vid_2");
+
+  let videoTime1 = 0;
+  let videoTime2 = 0;
 
   function getById(id) {
     return document.getElementById(id);
@@ -12,4 +17,49 @@
     );
   });
 
+  galleryVid1.addEventListener("click", function () {
+    if (!this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2) {
+      this.play();
+    }
+  });
+
+  galleryVid1.addEventListener("mouseover", function () {
+    this.setAttribute("controls","controls");
+  });
+
+  galleryVid1.addEventListener("mouseleave", function () {
+    this.removeAttribute("controls");
+  });
+
+  galleryVid2.addEventListener("click", function () {
+    if (!this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2) {
+      this.play();
+    }
+  });
+
+  galleryVid2.addEventListener("mouseover", function () {
+    this.setAttribute("controls","controls");
+  });
+
+  galleryVid2.addEventListener("mouseleave", function () {
+    this.removeAttribute("controls");
+  });
+
+
+  function openFullscreen(myVideo) {
+    console.log("hitting")
+    var elem = myVideo
+    console.log(elem)
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+      elem.msRequestFullscreen();
+    }
+  }
+  
+  
 })();
