13c3c499dfc8177ed228f926e39d5a853b8b1066
muadz-ict <muadz@klikpeta.com>
2021-03-11 22:30:20 +0700

    wip covid payment

--- app/js/script.js
+++ app/js/script.js
@@ -1,65 +1,63 @@
 (function () {
+  const $ = require("jquery");
   const calendarBtn = getById("g-calendar-btn");
   const galleryVid1 = getById("gallery_vid_1");
   const galleryVid2 = getById("gallery_vid_2");
 
-  let videoTime1 = 0;
-  let videoTime2 = 0;
-
   function getById(id) {
-    return document.getElementById(id);
+    return $(`#${id}`);
   }
 
-  calendarBtn.addEventListener("click", function () {
+  calendarBtn.click(function () {
     window.open(
       "https://www.google.com/calendar/render?action=TEMPLATE&text=Undangan+Pernikahan&details=Undangan+Pernikahan&dates=20201118T053000Z%2F20201118T083000Z",
       "_blank"
     );
   });
 
-  galleryVid1.addEventListener("click", function () {
-    if (!this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2) {
+  galleryVid1.click(function () {
+    if (
+      !this.currentTime > 0 &&
+      !this.paused &&
+      !this.ended &&
+      this.readyState > 2
+    ) {
       this.play();
     }
   });
 
-  galleryVid1.addEventListener("mouseover", function () {
-    this.setAttribute("controls","controls");
+  galleryVid1.mouseover(function () {
+    this.setAttribute("controls", "controls");
   });
 
-  galleryVid1.addEventListener("mouseleave", function () {
+  galleryVid1.mouseleave(function () {
     this.removeAttribute("controls");
   });
 
-  galleryVid2.addEventListener("click", function () {
-    if (!this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2) {
+  galleryVid2.click(function () {
+    if (
+      !this.currentTime > 0 &&
+      !this.paused &&
+      !this.ended &&
+      this.readyState > 2
+    ) {
       this.play();
     }
   });
 
-  galleryVid2.addEventListener("mouseover", function () {
-    this.setAttribute("controls","controls");
+  galleryVid2.mouseover(function () {
+    this.setAttribute("controls", "controls");
   });
 
-  galleryVid2.addEventListener("mouseleave", function () {
+  galleryVid2.mouseleave(function () {
     this.removeAttribute("controls");
   });
 
-
-  function openFullscreen(myVideo) {
-    console.log("hitting")
-    var elem = myVideo
-    console.log(elem)
-    if (elem.requestFullscreen) {
-      elem.requestFullscreen();
-    } else if (elem.mozRequestFullScreen) { /* Firefox */
-      elem.mozRequestFullScreen();
-    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-      elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE/Edge */
-      elem.msRequestFullscreen();
+  $("input[type='radio']").click(function () {
+    var radioValue = $("input[name='tab-input']:checked").val();
+    if (radioValue) {
+      // alert("Your are a - " + radioValue);
+      console.log(radioValue, typeof radioValue);
     }
-  }
-  
-  
+  });
 })();
